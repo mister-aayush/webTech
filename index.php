77a806a1107a598da65df2df730e87bce61b90c9
Aayush Shrestha <learn.aayush@gmail.com>
2025-12-13 18:02:58 +0545

    variable swaping domne

--- index.php
+++ index.php
@@ -28,4 +28,17 @@ $num =1;
         }
         echo "<br>";
     }
+
+    $a = 10;
+$b = 20;
+
+echo "Before Swapping:<br>";
+echo "a = $a , b = $b<br><br>";
+
+$a = $a + $b;
+$b = $a - $b;
+$a = $a - $b;
+
+echo "After Swapping:<br>";
+echo "a = $a , b = $b";
 ?>
