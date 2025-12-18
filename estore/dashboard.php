71c579e0ba8f1861d35de5252a55be322028fedb
Aayush Shrestha <learn.aayush@gmail.com>
2025-12-18 10:15:58 +0545

    minor cahnges

--- estore/dashboard.php
+++ estore/dashboard.php
@@ -1,6 +1,12 @@
 <?php 
 session_start();
-include 'header.php';
+include 'header.php';?>
+<div class="content">
+    <?php 
+    $user = $_SESSION['user'];
+    echo 'Welcome $user';
+    ?>
+</div>
 echo "LOgin secessful";
 include 'footer.php';
 ?>
