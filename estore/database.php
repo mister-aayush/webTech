529d85649a8de92297cbce6897ff45ab3bd89564
Aayush Shrestha <learn.aayush@gmail.com>
2025-12-24 10:16:46 +0545

    registeration complete

--- estore/database.php
+++ estore/database.php
@@ -2,7 +2,7 @@
 $db_host= "localhost";
 $db_user= "root";
 $db_password= "";
-$db_name = "jheu_store";
+$db_name = "jheegu_store_db";
 
 $dbconnection= mysqli_connect($db_host,$db_user,$db_password,$db_name);
 if(!$dbconnection){
--- estore/registration_process.php
+++ estore/registration_process.php
@@ -1,4 +1,5 @@
 <?php
+require 'database.php';
 if(isset($_POST)){
     //valdation
     $name = $phone=$email = $password= "";
@@ -8,11 +9,25 @@ if(isset($_POST)){
         header("Location: register.php? name_empty=1");
     }
 
-    echo "<pre>";
-var_dump($_POST);
-echo "</pre>";
+ $name = $_POST['full_name'];
+ $phone = $_POST['phone'];
+ $email = $_POST['email'];
+ $password = $_POST['password'];
 
-exit();
+// insert name ohone email pw in db
+
+$query = "INSERT INTO user(name,phone,email,password)
+            values('$name','$phone','$email','$password')";
+$res = mysqli_query($dbconnection,$query);
+if($res){
+
+   header("Location: login.php");
+}else{
+    echo "reigster unsecessful";
+}
+
+
+
 }else{
     header("Location: register.php");
 }
