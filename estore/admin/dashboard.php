f70e754a1ee1a5a502012cd33143aff4d1087073
Aayush Shrestha <learn.aayush@gmail.com>
2026-01-02 22:08:55 +0545

    login page done , dashboard table

--- estore/admin/dashboard.php
+++ estore/admin/dashboard.php
@@ -1,16 +1,31 @@
 <?php
 include 'header.php';
 $is_LoggedIn =  $_SESSION['is_loggedin'] ;
-echo $is_LoggedIn;
+
  
 if($is_LoggedIn == 1 ){
+?>
+  <table border="1" >
+
+   <thead>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Price</th>
+      <th>Quantity</th>
+      <th>Image</th>
+   </thead>
+    
+   <tr>
+      <td>hello </td>
+      <td>hello </td>
+      <td>hello </td>
+      <td>hello </td>
+      <td>hello </td>
+    </tr>
+  </table>
 
 
-
-
-  echo "thisis dashboard";
-
-  
+  <?php
 }else{
   
   header("Location: login.php");
