bd7cb00ddf1a6c6e4e7967fbf910abbc31ce2012
Aayush Shrestha <learn.aayush@gmail.com>
2026-01-04 23:26:27 +0545

    table fixing

--- estore/admin/dashboard.php
+++ estore/admin/dashboard.php
@@ -10,6 +10,10 @@ $is_LoggedIn =  $_SESSION['is_loggedin'];
 // die;
 
 require_once 'database.php';
+if (!isset($_SESSION['is_loggedin']) || $_SESSION['is_loggedin'] != 1) {
+  header("Location: login.php");
+  exit();
+}
 
 
 
@@ -19,32 +23,33 @@ $res = mysqli_query($dbconnection, $query);
 
 if ($is_LoggedIn == 1) {
 ?>
-  <div class="display-Table">
-  <table>
-  
-    <?php 
-    while ($row = mysqli_fetch_assoc($res)) {
-      $datas[] = $row;
-    }foreach ($datas as $item): ?>
+  <div class="display-table">
+    <table>
 
-      <thead>
-        <th>Name</th>
-        <th>Description</th>
-        <th>Price</th>
-        <th>Quantity</th>
-        <th>Image</th>
-      </thead>
+      <?php
+      $datas = [];
+      while ($row = mysqli_fetch_assoc($res)) {
+        $datas[] = $row;
+      }
+      ?>
 
-      <tr>
-        <td><?php echo $item['name']; ?> </td>
-        <td><?php echo $item['description']; ?> </td>
-        <td><?php echo $item['price']; ?> </td>
-        <td><?php echo $item['quantity']; ?> </td>
-        <td><?php echo $item['image']; ?> </td>
-
-      </tr>
-    <?php endforeach; ?>
-  </table>
+      <?php foreach ($datas as $item): ?>
+        <tr>
+          <td><?php echo $item['name']; ?></td>
+          <td><?php echo $item['description']; ?></td>
+          <td><?php echo $item['price']; ?></td>
+          <td><?php echo $item['quantity']; ?></td>
+          <td>
+            <img src="uploads/<?php echo $item['image']; ?>" width="60">
+          </td>
+          <td>
+            <a href="edit-product.php?id=1">
+              Edit
+            </a>
+          </td>
+        </tr>
+      <?php endforeach; ?>
+    </table>
   </div>
 
 <?php
